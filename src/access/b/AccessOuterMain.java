99bef5cea3285b2687a89b80a97d7ae91d8aea4d
Heeran Kim <heerankim3@gmail.com>
2024-07-21 17:45:31 +1000

    Section 6: Access modifier

---
+++ src/access/b/AccessOuterMain.java
@@ -0,0 +1,23 @@
+package access;
+
+import access.a.AccessData;
+
+public class AccessOuterMain {
+
+    public static void main(String[] args) {
+        AccessData data = new AccessData();
+
+        data.publicField = 1;
+        data.publicMethod();
+
+        /*
+        data.defaultField = 2;
+        data.defaultMethod();*/
+
+        /*
+        data.privateField = 3;
+        data.privateMethod();*/
+
+        data.innerAccess(); // public
+    }
+}
---
+++ src/access/ex/MaxCounter.java
@@ -0,0 +1,2 @@
+package access.ex;public class MaxCounter {
+}
