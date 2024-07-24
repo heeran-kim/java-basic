ab10ae93ca794860e84acd6674b3d4efe6c471a9
Heeran Kim <heerankim3@gmail.com>
2024-07-25 07:04:03 +1000

    necessity of abstract class and method

--- src/poly/ex2/Animal.java
+++ src/poly/ex2/Animal.java
@@ -1,2 +1,12 @@
-package poly.ex2;public class Animal {
+package poly.ex2;
+
+public class Animal {
+
+    protected Animal() {
+        // This parent class cannot be used as an object.
+    }
+
+    public void sound() {
+        System.out.println("Animal Sound");
+    }
 }
